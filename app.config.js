9a5c9769ea4c882ed25273ac3ff0f946c1aaebcd
Jaime Flores <jaimelopesflores@gmail.com>
2016-10-14 13:54:51 -0300

    MongoDB

--- app.config.js
+++ app.config.js
@@ -2,6 +2,6 @@ module.exports = {
   port: process.env.PORT || '3000',
   token_tag: process.env.TOKEN_TAG || 'qmagico',
   token_secret: process.env.TOKEN_SECRET || 'qmagico_challenge',
-  mockData: true,
+  mockData: false,
   mongoUrl: 'mongodb://demo:demo@ds035846.mlab.com:35846/qmagico-challenge'
 }
